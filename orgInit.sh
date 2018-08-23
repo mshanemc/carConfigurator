491519fef1665358cd6a585f7d08b9db07afb617
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-23 16:14:28 -0500

    refactor out boilerplate AND community bits

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx shane:github:package:install -g mshanemc -r community-boilerplate
+sfdx shane:github:src:install -g mshanemc -r customer-community-base
 sfdx force:source:push
 sfdx force:org:open -p lightning/setup/SetupNetworks/home
